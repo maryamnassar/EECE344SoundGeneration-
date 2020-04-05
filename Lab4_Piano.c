15a25c8802ba236bfce7c661fa3001502b5129a1
maryamnassar <49885459+maryamnassar@users.noreply.github.com>
2020-04-04 21:54:20 -0700

    Add files via upload

---
+++ Lab4_Piano.c
@@ -0,0 +1,40 @@
+// Lab4.c
+// Runs on TM4C123
+// Program written by: put your names here
+// Date Created: 
+// Last Modified:  
+// Lab number: 5
+// Brief description of the program
+//  
+// Hardware connections
+
+
+#include "tm4c123gh6pm.h"
+#include "PLL.h"
+#include "Sound.h"
+#include "Piano.h"
+#include "dac.h"
+
+// basic functions defined at end of startup.s
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void WaitForInterrupt(void);
+
+int main(void){   
+		unsigned long Data; 
+ PLL_Init();          // bus clock at 80 MHz
+ // all initializations go here  
+	EnableInterrupts();
+	DAC_Init();
+	Piano_Init(); 
+	//Sound_Init(); 
+	
+	
+  while(1){                
+
+	WaitForInterrupt();		
+		
+  }             
+}
+
+
